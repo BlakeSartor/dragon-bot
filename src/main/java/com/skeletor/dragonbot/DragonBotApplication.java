63da4bc352035d3eeba4a529db7d137583581644
Skeletor <blakesartor@gmail.com>
2024-03-30 14:57:50 -0500

    Initial commit

---
+++ src/main/java/com/skeletor/dragonbot/DragonBotApplication.java
@@ -0,0 +1,13 @@
+package com.skeletor.dragonbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skeletor/dragonbot/DragonBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeletor.dragonbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
